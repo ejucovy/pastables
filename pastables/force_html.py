f1be2f9c0cf5c0bb064ff8aa68cf305b07be4ab2
ejucovy <ejucovy@e0e46c49-be69-4f5a-ad62-21024a331aea>
2009-12-26 05:06:01 +0000

    * don't copy the response, just use it  * configurable forced content type
    git-svn-id: https://my-svn.assembla.com/svn/arbez/pastables/trunk@1404 e0e46c49-be69-4f5a-ad62-21024a331aea

--- pastables/force_html.py
+++ pastables/force_html.py
@@ -1,16 +1,22 @@
-def filter_factory(*args):
+def filter_factory(content_type=None, *args):
     def filter(app):
-        return ForceHtml(app)
+        return ForceHtml(app, content_type=content_type)
     return filter
 
 from webob import Request
 
 class ForceHtml(object):
-    def __init__(self, app):
+
+    content_type = 'text/html'
+
+    def __init__(self, app, content_type=None):
         self.app = app
+        self.content_type = content_type or self.content_type
 
     def __call__(self, environ, start_response):
         req = Request(environ)
-        res = req.get_response(self.app)
-        res.content_type = 'text/html'
+
+        res = self.app(environ, start_response)
+        res.content_type = self.content_type
+
         return res(environ, start_response)
