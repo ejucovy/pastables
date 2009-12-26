1ad85efc1b0c250657a2630000418f528e91626b
ejucovy <ejucovy@e0e46c49-be69-4f5a-ad62-21024a331aea>
2009-12-26 05:34:22 +0000

    HTTP_HOST dispatcher
    git-svn-id: https://my-svn.assembla.com/svn/arbez/pastables/trunk@1408 e0e46c49-be69-4f5a-ad62-21024a331aea

--- pastables/domain.py
+++ pastables/domain.py
@@ -1,15 +1,20 @@
 from webob.exc import *
 
 def composite_factory(loader, global_conf, **local_conf):
-    return MethodDispatcher()
+    apps = {}
+    for host, appname in local_conf.items():
+        assert host not in apps, "Duplicate assignment for host %s" % s
+        apps[host] = loader.get_app(appname)
+    return MethodDispatcher(apps)
     
+# TODO: default?
 class MethodDispatcher(object):
+    def __init__(self, apps):
+        self.hostmap = dict(apps) # copy
+        
     def __call__(self, environ, start_response):
-        import pdb; pdb.set_trace()
-        method = environ['REQUEST_METHOD']
-        if method == "GET":
-            return self.get(environ, start_response)
-        if method == "POST":
-            return self.post(environ, start_response)
+        host = environ['HTTP_HOST'].split(':')[0]
+        if host in self.hostmap:
+            return self.hostmap[host](environ, start_response)
 
-        return HTTPMethodNotAllowed()(environ, start_response)
+        return HTTPNotFound()(environ, start_response)
