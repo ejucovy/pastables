7496e18039ea6eb8ae2f8b47a8aa81821d7ffcee
ejucovy <ejucovy@e0e46c49-be69-4f5a-ad62-21024a331aea>
2010-01-05 18:29:23 +0000

    fix signature typo. add subclass which issues a basicauth challenge. extend filterfactory -- now user can add basic_realm=foo to the .ini section to select this component rather than the default.
    git-svn-id: https://my-svn.assembla.com/svn/arbez/pastables/trunk@1512 e0e46c49-be69-4f5a-ad62-21024a331aea

--- pastables/remote_user.py
+++ pastables/remote_user.py
@@ -1,13 +1,18 @@
-def filter_factory(global_conf, user_whitelist=None, user_blacklist=None):
+def filter_factory(global_conf, user_whitelist=None, user_blacklist=None, basic_realm=None):
     if user_whitelist:
         user_whitelist = user_whitelist.split()
     if user_blacklist:
         user_blacklist = user_blacklist.split()
 
     def filter(app):
+        if basic_realm is not None:
+            return Filter(
+                app,
+                whitelist=user_whitelist, blacklist=user_blacklist,
+                realm=basic_realm)
         return Filter(
             app,
-            user_whitelist=user_whitelist, user_blacklist=user_blacklist)
+            whitelist=user_whitelist, blacklist=user_blacklist)
     return filter
 
 from webob import Request
@@ -45,10 +50,23 @@ class Filter(object):
     @property
     def default_app(self):
         return exc.HTTPUnauthorized()
-
+    
     def __call__(self, environ, start_response):
         req = Request(environ)
 
         if self.filter.match(req):
             return self.app(environ, start_response)
-        return self.default_app(environ, start_response)
+        ret = self.default_app(environ, start_response)
+        return ret
+
+class BasicAuthFilter(Filter):
+    def __init__(self, app, whitelist, blacklist, realm):
+        Filter.__init__(self, app, whitelist, blacklist)
+        self.realm = realm
+
+    @property
+    def default_app(self):
+        ret = exc.HTTPUnauthorized()
+        ret.headers['WWW-Authenticate'] = 'Basic realm="%s"' % self.realm
+        return ret
+        
