2ba9cf39a27f19d0ab3a6e3f4c04f79fa419236d
ejucovy <ejucovy@e0e46c49-be69-4f5a-ad62-21024a331aea>
2010-01-06 04:04:29 +0000

    a domain mapper that makes more sense as far as i'm concerned
    git-svn-id: https://my-svn.assembla.com/svn/arbez/pastables/trunk@1525 e0e46c49-be69-4f5a-ad62-21024a331aea

--- pastables/domain.py
+++ pastables/domain.py
@@ -1,16 +1,54 @@
+"""
+{{{
+[composite:domains]
+use = egg:pastables#domain
+somanywhales.com = whales
+worldofbears.net = bears
+your.worldofbears.net = totally-different-bears
+zebra-associates.org = zebra
+
+[composite:bears]
+use = egg:pastables#strictdomain
+edit.worldofbears.net = bears-edit
+upload.worldofbears.net = bears-upload
+worldofbears.net = bears-published
+*.worldofbears.net = default-bears
+}}}
+
+currently what we have is neither.
+"""
+
 from webob.exc import *
 
 def composite_factory(loader, global_conf, **local_conf):
-    apps = {}
-    for host, appname in local_conf.items():
-        assert host not in apps, "Duplicate assignment for host %s" % s
-        apps[host] = loader.get_app(appname)
-    return MethodDispatcher(apps)
+    domains = generate_hostmap(local_conf)
+    return MethodDispatcher(domains)
+
+def strict_composite_factory(loader, global_conf, **local_conf):
+    domains = generate_hostmap(local_conf)
+    return MethodDispatcher(domains, loose=False)
+
     
+def generate_hostmap(local_conf):
+    domains = {}
+    for host, appname in local_conf.items():
+
+        domain = '.'.join(host.split('.')[-2:])
+        subdomain = '.'.join(host.split('.')[:-2])
+
+        if domain not in domains: domains[domain] = {}
+
+        assert subdomain not in domains[domain], "Duplicate assignment for host %s" % host
+
+        domains[domain][subdomain] = loader.get_app(appname)
+
+    
+
 # TODO: default?
-class MethodDispatcher(object):
-    def __init__(self, apps):
-        self.hostmap = dict(apps) # copy
+class LiteralHostDispatcher(object):
+    def __init__(self, apps, loose=True):
+        self.hostmap = apps
+        self.loose = loose
 
     # XXX overridable in config somehow?
     @property
@@ -19,8 +57,22 @@ class MethodDispatcher(object):
 
     def __call__(self, environ, start_response):
         host = environ['HTTP_HOST'].split(':')[0]
-        if host in self.hostmap:
-            return self.hostmap[host](environ, start_response)
 
+        domain = '.'.join(host.split('.')[-2:])
+        subdomain = '.'.join(host.split('.')[:-2])
+
+        if domain not in self.hostmap:
+            return self.default_app(environ, start_response)
+
+        domainmap = self.hostmap[domain]
+        if subdomain in domainmap:
+            return domainmmap[subdomain](environ, start_response)
+
+        if not self.loose:
+            if "*" in domainmap:
+                return domainmap["*"](environ, start_response)
+            return self.default_app(environ, start_response)
+
+        if '' in domainmap:
+            return domainmap[''](environ, start_response)
         return self.default_app(environ, start_response)
-
