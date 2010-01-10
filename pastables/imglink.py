1d819306d6b5fb175582fd4dd2a0de68a1581d90
ejucovy <ejucovy@e0e46c49-be69-4f5a-ad62-21024a331aea>
2010-01-10 06:08:43 +0000

    make it useable -- ignore <img/> subrequests; ignore requests to the raw link; ignore responses that aren't typed as images
    git-svn-id: https://my-svn.assembla.com/svn/arbez/pastables/trunk@1545 e0e46c49-be69-4f5a-ad62-21024a331aea

--- pastables/imglink.py
+++ pastables/imglink.py
@@ -1,4 +1,4 @@
-from webob import Response
+from webob import Request, Response
 
 class ImgLinkFilter(object):
     def __init__(self, app, raw_link):
@@ -8,9 +8,21 @@ class ImgLinkFilter(object):
     def __call__(self, environ, start_response):
         location = environ['PATH_INFO']
         raw_link = self.raw_link % location
-        return Response("""<a href="%s"><img src="%s" /></a>""" % (raw_link, raw_link))
+        req = Request(environ)
+        res = req.get_response(self.app)
 
-def filter_factory(global_conf, **kw):
+        if req.path_qs == raw_link:
+            return res(environ, start_response)
+        if environ.get('HTTP_ACCEPT', '').startswith('image/'):
+            return res(environ, start_response)
+
+        if res.content_type.startswith('image/'):
+            return Response("""<a href="%s"><img src="%s" /></a>""" % (raw_link, raw_link)
+                            )(environ, start_response)
+
+        return res(environ, start_response)
+
+def filter_factory(global_conf, raw_link=None, **kw):
     def filter(app):
-        return ImgLinkFilter(app)
+        return ImgLinkFilter(app, raw_link)
     return filter
