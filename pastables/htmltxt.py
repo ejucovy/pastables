7f47697928b498993a77a7288c64db91a3955859
ejucovy <ejucovy@e0e46c49-be69-4f5a-ad62-21024a331aea>
2010-01-10 05:21:49 +0000

    escape html characters in content
    git-svn-id: https://my-svn.assembla.com/svn/arbez/pastables/trunk@1543 e0e46c49-be69-4f5a-ad62-21024a331aea

--- pastables/htmltxt.py
+++ pastables/htmltxt.py
@@ -1,4 +1,5 @@
 from webob import Request
+import cgi
 
 class PreTextFilter(object):
     def __init__(self, app):
@@ -9,7 +10,7 @@ class PreTextFilter(object):
         res = req.get_response(self.app)
         if res.status_int == 200 and res.content_type == 'text/plain':
             res.content_type = 'text/html'
-            res.body = "<pre>%s</pre>" % res.body
+            res.body = "<pre>%s</pre>" % cgi.escape(res.body)
         return res(environ, start_response)
 
 def filter_factory(global_conf, **kw):
